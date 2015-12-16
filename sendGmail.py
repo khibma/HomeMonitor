6b65e942901406f2c8599beaa20c4c70a4e80e2f
Kevin Hibma <khibma@gmail.com>
2015-12-16 10:33:09 -0500

    email gmail oauth
    thanks @ghisprince

---
+++ sendGmail.py
@@ -0,0 +1,102 @@
+import httplib2
+import os
+import httplib
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+import email.mime.text
+import base64
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'pyscript'
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'gmail-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatability with Python 2.6
+            credentials = tools.run(flow, store)
+        print(('Storing credentials to ' + credential_path))
+    return credentials
+
+def CreateMessage(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+
+    from email.mime.text import MIMEText
+    message = email.mime.text.MIMEText(message_text,  )
+
+    message['from'] = sender
+    message['to'] = to
+    message['subject'] = subject
+    #return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode() }  #py3
+    return {'raw': base64.urlsafe_b64encode(message.as_string()).decode() }  #py2
+
+
+def SendMessage(service, user_id, message):
+    """Send an email message.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      message: Message to be sent.
+    Returns:
+      Sent Message.
+    """
+
+    message = (service.users().messages().send(userId=user_id, body=message).execute())
+    print(('Message Id: %s' % message['id']))
+    return message
+
+def sendEmail(frm, to, subjectline="empty subject", msgbody="empty body"):
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    message = CreateMessage(frm, to, subjectline, msgbody )
+    SendMessage(service, frm, message)
+
+if __name__ == '__main__':
+    sendEmail()
